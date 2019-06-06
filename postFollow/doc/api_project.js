ea6125d96fb6a6eabd60900c6aad8c1aeada8a95
zaici <362428028@qq.com>
2019-06-06 09:03:20 +0800

    <fix>修改目录结构

---
+++ postFollow/doc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-06-06T00:58:02.692Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
